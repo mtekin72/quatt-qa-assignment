d4f8e70a7fbdf5d77f5d81c70e42f6722e515f1b
mtekin72 <mtekin72@gmail.com>
2024-12-03 23:15:28 +0100

    export

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,12 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://gorest.co.in/public/v2',
     specPattern: 'cypress/integration/**/*.js', // Ensure this matches your file path,
-    projectId: "1cgwu4"
+    projectId: "1cgwu4",
+    
+      "env": {
+        "CYPRESS_API_TOKEN": process.env.CYPRESS_API_TOKEN
+      }
   },
+    
+  
 });
