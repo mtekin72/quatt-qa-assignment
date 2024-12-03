1d05f34203f95c89a8fd239f8ca4b3f0def7f12a
mtekin72 <mtekin72@gmail.com>
2024-12-03 20:50:30 +0100

    api token refresh

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,6 +7,6 @@ Cypress.Commands.add('apiRequest', (method, url, body, token) => {
             'Content-Type': 'application/json'
         },
         body,
-        failOnStatusCode: false // If you do not want status codes to cause failures pass the option: failOnStatusCode: false
+        failOnStatusCode: false // If you do not want status codes to cause failures pass the option: failOnStatusCode: false..
     });
 });
